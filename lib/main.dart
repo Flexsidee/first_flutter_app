f75857b4a7441b8e2c3b8d7af24230e106c77256
flexsidee <flexsidee@gmail.com>
2023-05-05 00:02:10 +0100

    started learning to style a widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Hello World!"),
+        backgroundColor: const Color.fromARGB(255, 81, 30, 128),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(244, 33, 2, 43),
+            Color.fromARGB(244, 33, 7, 43)
+          ])),
+          child: const Center(
+            child: Text(
+              "Hello World!",
+              style: TextStyle(color: Colors.white, fontSize: 50),
+            ),
+          ),
         ),
       ),
     ),
